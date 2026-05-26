bb9eaa436cc3e7ab0d9e84537a39edb2d5370814
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-05-26 21:34:01 +0500

    Add AI task: Universitetga qabulning GPA chegaralari

---
+++ ai_topshiriqlar/Universitetga_qabulning_GPA_chegaralari.py
@@ -0,0 +1,13 @@
+# Universitetga qabulning GPA chegaralari
+# Kurs: IT Dasturlash
+# Mavzu: Solishtirish operatorlari: ==, !=, >, <, >=, <=
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+name, gpa = input().split()
+gpa = float(gpa)
+if 2.0 <= gpa <= 3.5:
+    print('Qabul qilindi')
+else:
+    print('Qabul qilinmadi')
