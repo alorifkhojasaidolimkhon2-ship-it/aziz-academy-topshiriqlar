2666d920612f5c249b8ea006a3636d866f2121c1
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-05-26 21:34:52 +0500

    Add AI task: Aviakompaniya yo'lovchi chiptalari chegaralari

---
+++ ai_topshiriqlar/Aviakompaniya_yolovchi_chiptalari_chegaralari.py
@@ -0,0 +1,15 @@
+# Aviakompaniya yo'lovchi chiptalari chegaralari
+# Kurs: IT Dasturlash
+# Mavzu: Solishtirish operatorlari: ==, !=, >, <, >=, <=
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+yosh = int(input())
+if yosh <= 5:
+    print('bepul')
+elif 5 < yosh < 18:
+    print('50% cheklangan')
+elif 18 < yosh < 65:
+    print("to'liq")
+elif yosh > 65:
+    print('20% cheklangan')
