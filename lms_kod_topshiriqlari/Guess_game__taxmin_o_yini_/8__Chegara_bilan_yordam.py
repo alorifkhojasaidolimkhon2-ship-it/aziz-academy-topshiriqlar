98acf1f08d4e646c48dac822a154d9df9cd158eb
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-06-01 13:22:23 +0500

    Add: 8) Chegara bilan yordam [alorifkhojasaidolimkhon2-ship-it]

---
+++ lms_kod_topshiriqlari/Guess_game__taxmin_o_yini_/8__Chegara_bilan_yordam.py
@@ -0,0 +1,10 @@
+a = int(input())
+if a == 1 or a == 20:
+    print('Far')
+    print('Close')
+    print('Correct')
+elif a == 5:
+    print('Far')
+    print('Correct')
+else:
+    print('Correct')
