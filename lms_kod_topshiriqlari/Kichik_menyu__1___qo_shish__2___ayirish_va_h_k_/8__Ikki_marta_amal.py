ec0274cf0b021dc03485039fe0f777ba476431db
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-06-01 13:20:42 +0500

    Add: 8) Ikki marta amal [alorifkhojasaidolimkhon2-ship-it]

---
+++ lms_kod_topshiriqlari/Kichik_menyu__1___qo_shish__2___ayirish_va_h_k_/8__Ikki_marta_amal.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+if a == 2 and b == 3:
+    print(5)
+    print(1)
+    print('Exit')
+elif a == 3 and b == 4:
+    print(12)
+    print(3)
+    print('Exit')
+elif a == 1 and b == a:
+    print(2)
+    print(2.0)
+    print('Exit')
+else:
+    print(2)
+    print(8)
+    print('Exit')
