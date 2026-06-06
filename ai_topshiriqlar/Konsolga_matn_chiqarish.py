9ec285e4a46f8e69c4c8959cb02741ac08d13bcc
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-06-06 20:29:36 +0500

    Add AI task: Konsolga matn chiqarish

---
+++ ai_topshiriqlar/Konsolga_matn_chiqarish.py
@@ -0,0 +1,13 @@
+# Konsolga matn chiqarish
+# Kurs: IT Dasturlash
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+ism = input()
+yosh = int(input())
+shahar = input()
+print(f'Sizning ismingiz: {ism}')
+print(f'Sizning yoshingiz: {yosh}')
+print(f"Sizning tug'ilgan shahringiz: {shahar}")
+print(f'Siz {yosh} yoshdasiz')
