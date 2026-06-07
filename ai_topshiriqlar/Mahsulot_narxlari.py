4747f82f7dd8ea3829c6f41100515c16332dd768
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-06-07 12:20:27 +0500

    Add AI task: Mahsulot narxlari

---
+++ ai_topshiriqlar/Mahsulot_narxlari.py
@@ -0,0 +1,19 @@
+# Mahsulot narxlari
+# Kurs: IT Dasturlash
+# Mavzu: 2-mavzu: Birinchi dastur, print() va kommentlar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a, b = input().split()
+n = int(input())
+c, d = input().split()
+y = int(input())
+found = False
+if n == 1:
+    print(a, b)
+    found = True
+if y == 1:
+    print(c, d)
+    found = True
+if not found:
+    print('Hech qanday mahsulot chiptalanmagan')
