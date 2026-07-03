dc0814e07001ad5c57303d8431f0057e32cfd3df
alorifkhojasaidolimkhon2-ship-it <alorifkhojasaidolimkhon2@gmail.com>
2026-07-03 17:12:38 +0500

    Add: Profil kartasi [alorifkhojasaidolimkhon2-ship-it]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-Profil_kartasi.py
@@ -0,0 +1,11 @@
+a = 'Aziz'
+b = 'Karimov'
+c = 'Python'
+d = "Boshlang'ich"
+e = 'Toshkent'
+print('=== PROFIL ===')
+print(f'Ism: {a}')
+print(f'Familiya: {b}')
+print(f'Kurs: {c}')
+print(f'Daraja: {d}')
+print(f'Shahar: {e}')
